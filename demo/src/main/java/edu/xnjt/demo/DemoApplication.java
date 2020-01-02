3ee86fc82cf867d21c69e11dbd5155d9dbbfff58
ASUS <ASUS@DESKTOP-OBCSFP6>
2020-01-02 14:20:39 +0800

    接口，接口文档及数据库文件

--- demo/src/main/java/edu/xnjt/demo/DemoApplication.java
+++ demo/src/main/java/edu/xnjt/demo/DemoApplication.java
@@ -1,8 +1,9 @@
 package edu.xnjt.demo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@MapperScan("edu.xnjt.demo.mapper")
 @SpringBootApplication
 public class DemoApplication {
 
